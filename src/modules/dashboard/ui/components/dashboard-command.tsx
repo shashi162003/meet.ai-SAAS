0a124fd93ee28d2651d6df609d45b73e6460fbd7
masterShashi <shashikumargupta443@gmail.com>
2025-07-03 18:51:35 +0530

    07: Dashboard Navbar

---
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -0,0 +1,20 @@
+import { CommandInput, CommandDialog, CommandList, CommandItem } from "@/components/ui/command";
+import { Dispatch, SetStateAction } from "react";
+
+interface Props {
+    open: boolean;
+    setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const DashboardCommand = ({open, setOpen}: Props) => {
+    return (
+        <CommandDialog open={open} onOpenChange={setOpen}>
+            <CommandInput placeholder="Find a meeting or agent" />
+            <CommandList>
+                <CommandItem>
+                    Test
+                </CommandItem>
+            </CommandList>
+        </CommandDialog>
+    );
+};
